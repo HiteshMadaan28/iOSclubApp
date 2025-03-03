6308c170050ee947360ba245fc5f52c8f25c9862
HiteshMadaan28 <hiteshmadaan28@gmail.com>
2025-03-04 00:47:05 +0530

    Added Demo Connection View

---
+++ iOSclub/Connections/View/ConnectionContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ConnectionContentView.swift
+//  iOSclub
+//
+//  Created by Hitesh Madaan on 03/03/25.
+//
+
+import SwiftUI
+
+struct ConnectionContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ConnectionContentView()
+}
