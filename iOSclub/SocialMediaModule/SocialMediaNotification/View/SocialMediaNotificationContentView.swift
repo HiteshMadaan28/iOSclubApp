fe0e685643c1d3377233b881ee9ad33022afb96c
HiteshMadaan28 <hiteshmadaan28@gmail.com>
2025-04-15 23:44:54 +0530

    Updated JobContentView

---
+++ iOSclub/SocialMediaModule/SocialMediaNotification/View/SocialMediaNotificationContentView.swift
@@ -0,0 +1,18 @@
+//
+//  SocialMediaNotificationContentView.swift
+//  iOSclub
+//
+//  Created by Hitesh Madaan on 15/04/25.
+//
+
+import SwiftUI
+
+struct SocialMediaNotificationContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SocialMediaNotificationContentView()
+}
